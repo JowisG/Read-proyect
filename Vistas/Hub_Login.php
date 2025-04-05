7653631c308bad9a970182424c5e851eee4c7312
JowisG <j.juegos0210@gmail.com>
2025-04-05 15:15:53 +0200

    Starting content of topics

--- Vistas/Hub_Login.php
+++ Vistas/Hub_Login.php
@@ -3,7 +3,27 @@
     <div class="divisor">
         <article class="manwha">
             <h2>Manwha</h2>
-            <p>Insert description</p>
+            <intro>
+                <p>For those who don’t know what a manhwa is it is simply the Korean version of a manga. It also differs in the media is presented in, instead of a comic is a long digital comic strip that you scroll in your phone.</p>
+                <p>I believe all of them are in color, which is a treat compared to the black and white style manga opts, don’t get me wrong I firmly think that the aesthetics manga has is great.</p>
+            </intro>
+            <section>
+                <h3>Kinds of stories to expect</h3>
+                <p>The manhwas have similar stories to mangas, but they usually feel different enough making them fresh and new. The stories you mostly are going to find are:</p>
+                <ul>
+                    <li>Strong protagonist in a fantasy world were monsters or other kinds of threats push the world to an unbalanced state</li>
+                    <li>Romance “isekai” or reverse time fantasy stories</li>
+                    <li>Academy or school manhwas</li>
+                </ul>
+                <p>There are always other types of stories but after a lot of stories, I think those are the most prominent types of stories, right now. They are not the only type of stories you may find, like in manga, but there is clearly an abundance of these kinds of stories.</p>
+            </section>
+            <section>
+                <h3>Remmendations</h3>
+                <p>Some noteworthy stories I found:</p>
+                <div class="recommend">
+                    <!-- Recomendaciones en un grid -->
+                </div>
+            </section>
             <div class="Hightlight">
                 <img src="#" alt="No img at the moment">
                 <p></p>
@@ -24,8 +44,10 @@
         <section class="Singup-Extra">
             <h3>Singup to get features</h3>
             <p>No need to singup with an email just a user and a password they must be unique in the data base.</p>
-            <button type="button">SingUp</button>
-            <button type="Login">Have an account<b>?</b></button>
+            <div class="btns">
+                <button type="button">SingUp</button>
+                <button type="button">Have an account<b>?</b></button>
+            </div>
         </section>
         <section class="About">
             <h3>About the development of the website</h3>
