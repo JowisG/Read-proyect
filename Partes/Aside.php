a381951cf87797173a01dff6d7b44bc0c2b02ef4
JowisG <j.juegos0210@gmail.com>
2025-04-12 16:11:52 +0200

    Structure

--- Partes/Aside.php
+++ Partes/Aside.php
@@ -1,3 +1,42 @@
 <aside>
-    <h2>Aside</h2>
+    <h2>Most recently updated</h2>
+    <section>
+        <article>
+            <h3>opinion</h3>
+            <img src="" alt="No img yet">
+            <p>description</p>
+            <a href="">link</a>
+        </article>
+        <article>
+            <h3>opinion</h3>
+            <img src="" alt="No img yet">
+            <p>description</p>
+            <a href="">link</a>
+        </article>
+        <article>
+            <h3>opinion</h3>
+            <img src="" alt="No img yet">
+            <p>description</p>
+            <a href="">link</a>
+        </article>
+        <article>
+            <h3>opinion</h3>
+            <img src="" alt="No img yet">
+            <p>description</p>
+            <a href="">link</a>
+        </article>
+    </section>
+    <h2>Hightlights</h2>
+    <section>
+        <article>
+            <h3>Hightlight 1</h3>
+            <p>Description</p>
+            <a href="">link</a>
+        </article>
+        <article>
+            <h3>Hightlight 2</h3>
+            <p>Description</p>
+            <a href="">link</a>
+        </article>
+    </section>
 </aside>
--- Vistas/opinions.php
+++ Vistas/opinions.php
@@ -1,9 +1,9 @@
 <main>
     <intro>
         <h1>Opinions</h1>
-        <p>Deciding what are you going to read with the incredible amount of stories there are is quite hard, so usually you end up with 2 options. You start skimming synopsis or reading the first chapters to see if you will like them; or/and see reviews or simple opinions of people to see if the story is any good.</p>
-        <p>This becomes quite tedious, and I feel like it is not going anywhere, but I will try to make it less of a pain by making more accessible opinions that talk more about the story without spoiling, just trying to transmit what you need to expect of the story.</p>
+        <p>Deciding what are you going to read with the incredible amount of stories there are is quite hard, so usually you end up with 2 options. You start skimming synopsis or reading the first chapters to see if you will like them; or/and see reviews or simple opinions of people to see if the story is any good. This becomes quite tedious, and I feel like it is not going anywhere, but I will try to make it less of a pain by making more accessible opinions that talk more about the story without spoiling, just trying to transmit what you need to expect of the story.</p>
     </intro>
+    <hr>
     <content>
         <?php
             include "./Partes/Aside.php";
