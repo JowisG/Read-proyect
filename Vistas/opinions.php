eba4f0776f884e17a20ccda5b917fe1a70f470d7
JowisG <j.juegos0210@gmail.com>
2025-04-12 15:17:24 +0200

    Content opinions and start aside

--- Vistas/opinions.php
+++ Vistas/opinions.php
@@ -1,7 +1,8 @@
 <main>
     <intro>
         <h1>Opinions</h1>
-        <p>Here you have some opinions of the stories I read. (Momentaneo)</p>
+        <p>Deciding what are you going to read with the incredible amount of stories there are is quite hard, so usually you end up with 2 options. You start skimming synopsis or reading the first chapters to see if you will like them; or/and see reviews or simple opinions of people to see if the story is any good.</p>
+        <p>This becomes quite tedious, and I feel like it is not going anywhere, but I will try to make it less of a pain by making more accessible opinions that talk more about the story without spoiling, just trying to transmit what you need to expect of the story.</p>
     </intro>
     <content>
         <?php
@@ -9,85 +10,85 @@
         ?>
         <div class="manhwa">
             <h2>Manhwas</h2>
-            <p>Description</p>
+            <p>I read a lot more manhwas than mangas (about an 80% more), so I believe I know which stories are worth the time. Most of the manhwas you will find here are the ones I think are incredible and you should give it a shot.</p>
             <section class="op">
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 1</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 1</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 2</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 2</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 3</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 3</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 4</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 4</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 5</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 5</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 6</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 6</a>
                 </article>
             </section>
         </div>
         <div class="manga">
             <h2>Mangas</h2>
-            <p>Description</p>
+            <p>Like I said earlier, I do not read as much manga as manhwa, so take my recommendations more personal than anything else. I do not read the usual popular stories, I am more into reading stories which don’t have an anime and are more average, but some of them are quite good.</p>
             <section class="op">
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 1</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 1</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 2</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 2</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 3</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 3</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 4</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 4</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 5</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 5</a>
                 </article>
                 <article>
-                    <h3></h3>
+                    <h3>Opinion 6</h3>
                     <img src="" alt="">
-                    <p></p>
-                    <a href="http://"></a>
+                    <p>Description</p>
+                    <a href="">Opinion 6</a>
                 </article>
             </section>
         </div>
